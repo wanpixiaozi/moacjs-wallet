a4e8350d4f7c389af036311f883edeeb2fc67466
caizhenglong <caizl2002@hotmail.com>
2018-12-11 14:19:59 +0800

    remove deprecated sha3 func

--- src/thirdparty.js
+++ src/thirdparty.js
@@ -1,5 +1,5 @@
 var Wallet = require('./index.js')
-var ethUtil = require('ethereumjs-util')
+var moacUtil = require('moacjs-util')
 var crypto = require('crypto')
 var scryptsy = require('scrypt.js')
 var utf8 = require('utf8')
@@ -85,7 +85,7 @@ function decodeCryptojsSalt (input) {
  * This wallet format is created by https://github.com/SilentCicero/ethereumjs-accounts
  * and used on https://www.myetherwallet.com/
  */
-Thirdparty.fromEtherWallet = function (input, password) {
+Thirdparty.fromMoacWallet = function (input, password) {
   var json = (typeof input === 'object') ? input : JSON.parse(input)
 
   var privKey
@@ -133,8 +133,8 @@ Thirdparty.fromEtherWallet = function (input, password) {
   return wallet
 }
 
-Thirdparty.fromEtherCamp = function (passphrase) {
-  return new Wallet(ethUtil.sha3(Buffer.from(passphrase)))
+Thirdparty.fromMoacCamp = function (passphrase) {
+  return new Wallet(moacUtil.keccak(Buffer.from(passphrase)))
 }
 
 Thirdparty.fromKryptoKit = function (entropy, password) {
@@ -176,13 +176,13 @@ Thirdparty.fromKryptoKit = function (entropy, password) {
 
   var privKey
   if (type === 'd') {
-    privKey = ethUtil.sha256(entropy)
+    privKey = moacUtil.sha256(entropy)
   } else if (type === 'q') {
     if (typeof password !== 'string') {
       throw new Error('Password required')
     }
 
-    var encryptedSeed = ethUtil.sha256(Buffer.from(entropy.slice(0, 30)))
+    var encryptedSeed = moacUtil.sha256(Buffer.from(entropy.slice(0, 30)))
     var checksum = entropy.slice(30, 46)
 
     var salt = kryptoKitBrokenScryptSeed(encryptedSeed)
@@ -210,7 +210,7 @@ Thirdparty.fromKryptoKit = function (entropy, password) {
     ])
 
     if (checksum.length > 0) {
-      if (checksum !== ethUtil.sha256(ethUtil.sha256(privKey)).slice(0, 8).toString('hex')) {
+      if (checksum !== moacUtil.sha256(moacUtil.sha256(privKey)).slice(0, 8).toString('hex')) {
         throw new Error('Failed to decrypt input - possibly invalid passphrase')
       }
     }
